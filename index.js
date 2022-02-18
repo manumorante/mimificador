720346d2730984e60986d0cf5071252ff18c5c16
Manu Morante <manu@estadologico.com>
2022-02-18 09:11:09 +0100

    JS when HTML load

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 function Mimificator() {
-  let inputText = '';
-  let outputText = '';
-  const textarea = document.querySelector('.js-textarea');
-  const output = document.querySelector('.js-output');
+  let inputText = ''
+  let outputText = ''
+  const textarea = document.querySelector('.js-textarea')
+  const output = document.querySelector('.js-output')
+  const btnAction = document.querySelector('.js-btn-action')
+
+  if(!textarea || !output || !btnAction) return false
 
   function mimify(stringParam) {
-    let t = stringParam || '';
-    t = t.toLowerCase();
-    t = t.replace(/qu/g, 'k');
-    t = t.replace(/ca|co|cu/g, 'ki');
-    t = t.replace(/[aeiou]/g, 'i');
-    t = t.replace(/[áéíóú]/g, 'í');
-    return t;
+    let t = stringParam || ''
+    t = t.toLowerCase()
+    t = t.replace(/qu/g, 'k')
+    t = t.replace(/ca|co|cu/g, 'ki')
+    t = t.replace(/[aeiou]/g, 'i')
+    t = t.replace(/[áéíóú]/g, 'í')
+    return t
   }
 
   function execute() {
-    inputText = textarea.value;
-    outputText = mimify(inputText);
-    output.innerHTML = outputText;
+    inputText = textarea.value
+    outputText = mimify(inputText)
+    output.innerHTML = outputText
   }
 
   // Events
-  document.querySelector('.js-btn-action').addEventListener('click', () => execute());
-  textarea.addEventListener('keyup', () => {execute();});
-  textarea.addEventListener('change', () => {execute();});
+  btnAction.addEventListener('click', () => execute())
+  textarea.addEventListener('keyup', () => {execute()})
+  textarea.addEventListener('change', () => {execute()})
 
-  execute();
+  execute()
 
-  new Clipboard('.js-copy');
+  new Clipboard('.js-copy')
 }
 
-Mimificator();
+window.onload = function() {
+  Mimificator()
+}
