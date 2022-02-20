e161b417a7ae9964ff58c7748e930272d56d3e0a
Manu Morante <manu@estadologico.com>
2022-02-20 18:37:26 +0100

    Copy to clipboard

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,8 @@ const App = () => {
     fitTextarea(e)
   }
 
-  const handleCopy = (e) => {
-    e.preventDefault()
-    // new Clipboard('.js-copy')
-    console.log('copied')
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(mimified);
   }
 
   return <div className="container mx-auto flex flex-col h-screen p-8 gap-7">
@@ -28,11 +26,11 @@ const App = () => {
       
     <div className="flex flex-col md:flex-row gap-3 md:h-1/2">
       <textarea onKeyDown={handleKeyDown} className="text-lg resize-none block w-full p-5 rounded-lg border-4 border-gray-700 focus:border-gray-600 bg-gray-800 md:h-full focus:outline-none" placeholder="Escribe algo..." autoComplete="off" />
-      <div className="text-lg resize-none block w-full p-5 rounded-lg border-4 border-sky-700 bg-sky-800 md:h-full focus:outline-none cursor-pointer break-all" title="Click para copiar">{mimified}</div>
+      <div onClick={copyToClipboard} className="text-lg resize-none block w-full p-5 rounded-lg border-4 border-sky-700 bg-sky-800 md:h-full focus:outline-none cursor-pointer break-all" title="Click para copiar">{mimified}</div>
     </div>
 
     <div>
-      <button onClick={handleCopy} className="w-full p-4 rounded-md bg-gray-700 hover:bg-gray-600" data-clipboard-action="copy" data-clipboard-target="#output">Copiar</button>
+      <button onClick={copyToClipboard} className="w-full p-4 rounded-md bg-gray-700 hover:bg-gray-600" data-clipboard-action="copy" data-clipboard-target="#output">Copiar</button>
     </div>
   </div>
 }
