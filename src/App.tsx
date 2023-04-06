3db8e263f85280e422a7b16d434df4fd3924bcc0
Manu <manu@estadologico.com>
2023-04-06 23:56:56 +0200

    Fine-tuning

--- src/App.tsx
+++ src/App.tsx
@@ -37,37 +37,66 @@ export default function App() {
 
   const buttonCx = cx(
     'py-3 px-4 rounded-md',
+    'text-white',
     'bg-gradient-to-tr from-emerald-500 to-emerald-700',
     'font-bold'
   )
 
-  const titleCx = cx('text-var-xl font-extrabold tracking-tight')
-  const descriptionCx = cx('text-var-lg font-normal leading-tight')
+  const titleCx = cx(
+    'text-var-2xl text-neutral-900 dark:text-neutral-50 font-extrabold tracking-tight'
+  )
+  const descriptionCx = cx('mb-3 text-var-xl font-normal leading-tight sm:leading-none ')
 
   const textareaCx = cx(
-    'block w-full p-5',
+    'block w-full p-3 xs:p-4 sm:p-5',
     'rounded-lg border-4 focus:outline-none',
-    'text-var-md',
     'resize-none',
     'transition-colors duration-500'
   )
   const inputCx = cx(
     textareaCx,
-    'border-gray-700 focus:border-gray-600 bg-gray-800/60 focus:bg-gray-800'
+    'text-var-md',
+
+    // Light mode
+    'bg-gray-50 border-gray-200',
+    'focus:bg-white focus:border-gray-300',
+
+    // Dark mode
+    'dark:bg-gray-800/60 dark:border-gray-600/50',
+    'dark:focus:bg-gray-800 dark:focus:border-gray-600'
   )
-  const outputCx = cx(textareaCx, 'border-sky-700 bg-sky-800')
-  const outputPlaceholderCx = cx(textareaCx, 'text-sky-500 border-sky-900 bg-sky-950')
+
+  const outputPlaceholderCx = cx(
+    textareaCx,
+    'text-var-md text-center',
+
+    // Light mode
+    'text-sky-500/60  bg-sky-100 border-sky-100',
+
+    // Dark mode
+    'dark:text-sky-500 dark:bg-sky-950 dark:border-sky-950'
+  )
+
+  const outputCx = cx(
+    textareaCx,
+    'text-var-lg text-center tracking-wide',
+
+    // Light mode
+    'text-sky-500  bg-sky-200 border-sky-200/50',
+
+    // Dark mode
+    'dark:text-sky-300 dark:bg-sky-900 dark:border-sky-900'
+  )
 
   return (
     <>
       <h1 className={titleCx}>{data.title}</h1>
 
       <h2 className={descriptionCx}>
-        Versiona texto a <strong>mi mi mi</strong> burlón reemplazando las vocales con{' '}
-        <strong>i</strong>
+        <span className='italic'>mi mi mi ...</span> {data.description}.
       </h2>
 
-      <div className='my-3 flex flex-col md:flex-row gap-3'>
+      <div className='flex flex-col md:flex-row gap-3'>
         <textarea
           ref={textareaRef}
           onKeyUp={hundleKeyUp}
@@ -82,19 +111,25 @@ export default function App() {
         ) : (
           <div className={outputCx}>{mimified}</div>
         )}
-
-        <div
-          onClick={handleClear}
-          className={cx(
-            'text-right transition duration-500',
-            { '-translate-y-4 opacity-0 cursor-default': !mimified },
-            { 'opacity-60 cursor-pointer': !!mimified }
-          )}>
-          Limpiar
-        </div>
       </div>
 
-      <section>
+      <div
+        onClick={handleClear}
+        className={cx(
+          'text-right transition duration-500',
+          { '-translate-y-4 opacity-0 cursor-default': !mimified },
+          { 'opacity-60 cursor-pointer': !!mimified }
+        )}>
+        Limpiar
+      </div>
+
+      <div className='sticky bottom-8 flex justify-center'>
+        <button onClick={handleCopy} className={buttonCx}>
+          {data.cta.text}
+        </button>
+      </div>
+
+      <section className='text-var-md mb-6'>
         <span>Ejemplos:</span>{' '}
         {data.examples.map((example, index) => (
           <>
@@ -109,12 +144,6 @@ export default function App() {
         ))}
         {'...'}
       </section>
-
-      <div className='sticky bottom-8 flex justify-center'>
-        <button onClick={handleCopy} className={buttonCx}>
-          {data.cta.text}
-        </button>
-      </div>
     </>
   )
 }
